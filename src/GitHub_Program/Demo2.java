0d478c2e919ce00ca7a382853ef652fef8322043
Avinash <Avinash@DESKTOP-56JABDL>
2021-08-02 18:20:50 +0530

    Avi: repository created

---
+++ src/GitHub_Program/Demo2.java
@@ -0,0 +1,14 @@
+package GitHub_Program;
+
+public class Demo2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("GitHub repository");
+		System.out.println("GitHub repository");
+		System.out.println("GitHub repository");
+
+
+	}
+}
+
